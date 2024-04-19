428d75454f1e68cce05aeb945f7767cffd349ec9
Vishvesh Pandey <vishvesh2590@gmail.com>
2024-04-19 13:30:01 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from "./components/Header";
 import Container1 from "./components/Container1";
 import Container2 from "./components/Container2";
 import Container3 from "./components/Container3";
-import "./app.css";
 
 function App(props) {
   const [showResister, setResister] = useState(false);
